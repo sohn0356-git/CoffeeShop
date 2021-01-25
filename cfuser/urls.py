71f057fef84a978ee79d459352c4460ebc8e5956
sohn0356-git <sohn0356@gmail.com>
2021-01-25 18:54:52 +0900

    Upload user's url and view

--- cfuser/urls.py
+++ cfuser/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include
+
 from django.conf.urls import url
 
+from cfuser.views import *
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^user/', include('cfuser.urls'))
+    url(r'^$', UserLV.as_view(), name='index')
 ]
--- cfuser/views.py
+++ cfuser/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from cfuser.models import Cfuser
 # Create your views here.
+
+class UserLV(ListView):
+    model = Cfuser
