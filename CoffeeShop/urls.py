adf9c64b3e8fa6e65302cdbb24163dc1ec8cb749
sohn0356-git <sohn0356@gmail.com>
2021-02-02 14:30:52 +0900

    Modify to connect template and view

--- CoffeeShop/urls.py
+++ CoffeeShop/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('cfuser.urls')),
     url(r'^board/', include('cfboard.urls')),
+    url(r'^summernote/', include('django_summernote.urls')),
 ]
